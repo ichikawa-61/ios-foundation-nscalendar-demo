eb84764633407140309d36cafa868e9248fecbf6
stv-ekushida <e.kushida@st-ventures.jp>
2017-04-04 12:10:02 +0900

    Update　テストコードを追加

--- ios-foundation-nscalendarTests/ios_foundation_nscalendarTests.swift
+++ ios-foundation-nscalendarTests/ios_foundation_nscalendarTests.swift
@@ -11,26 +11,66 @@ import XCTest
 
 class ios_foundation_nscalendarTests: XCTestCase {
     
+    private let tokyoTimeZone = "Asia/Tokyo"
+    private let jpLocal = "ja"
+    private var calendar = NSCalendar.current
+    private let helper = CalendarHelper()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        helper.setLocal(local: jpLocal)
+        helper.setTimeZone(timeZone: tokyoTimeZone)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    /// 年を取得する
+    func testDateYear() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        XCTAssertEqual(helper.dateYear(date: date), 2017)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    /// 月を取得する
+    func testDateMonth() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        XCTAssertEqual(helper.dateMonth(date: date), 1)
+    }
+
+    /// 日を取得する
+    func testDateDay() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        XCTAssertEqual(helper.dateDay(date: date), 2)
+    }
+
+    /// 時間を取得する
+    func testDateHour() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        XCTAssertEqual(helper.dateHour(date: date), 3)
+    }
+
+    /// 分を取得する
+    func testDateMinite() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        XCTAssertEqual(helper.dateMinute(date: date), 4)
+    }
+    
+    /// 曜日を取得する
+    func testDateWeekDay() {
+        let date = str2Date(dateStr: "2017/1/2 3:4")
+        let weekDayIndex = helper.dateWeekDay(date: date)
+        let weekDays = helper.renderShortWeekdaySymbols()
+        XCTAssertEqual(weekDays[weekDayIndex], "火")
+    }
+    
+    //MARK:Helper
+    func str2Date(dateStr: String) -> Date {
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP");
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        return formatter.date(from: dateStr)!
     }
     
 }
